f03f21a091b5337f1cb71471dd5690e034697095
Di Lu <brotherludi@hotmail.com>
2023-02-14 21:32:07 -0700

    Accept arguments that will enable filtering

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -91,19 +91,67 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = (options, limit = 10) => {
+  
+  //Setup an array to hold any parameters that may be available for the query.
+  const queryParams = [];
+
+  //Start the query with all information that comes before the WHERE clause.
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  let stringCommand = 'WHERE';
+
+  //Check if a city has been passed in as an option. Add the city to the params array and create a WHERE clause for the city.
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `${stringCommand} city LIKE $${queryParams.length} `;
+    stringCommand = 'AND';
+  }
+
+  if (options.owner_id) {
+    queryParams.push(options.owner_id);
+    queryString += `${stringCommand} owner_id = $${queryParams.length} `;
+    stringCommand = 'AND';
+  }
+
+  if (options.minimum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100);
+    queryString += `${stringCommand} cost_per_night >= $${queryParams.length} `;
+    stringCommand = 'AND';
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(options.maximum_price_per_night * 100);
+    queryString += `${stringCommand} cost_per_night <= $${queryParams.length} `;
+    stringCommand = 'AND';
+  }
+
+  queryString += `
+  GROUP BY properties.id`;
+
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    queryString += ` HAVING avg(property_reviews.rating) >= $${queryParams.length} `;
+  }
+
+  //Add any query that comes after the WHERE clause.
+  queryParams.push(limit);
+  queryString += `
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
   return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => {
-      //console.log(result.rows);
-      return result.rows;
-    })
-    .catch((err) => {
+    .query(queryString, queryParams)
+    .then((res) => res.rows)
+    .catch(err => {
       console.log(err.message);
     });
 };
-
 exports.getAllProperties = getAllProperties;
-
 
 /**
  * Add a property to the database
